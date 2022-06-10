da04d10d3f67dadf4a6aa61ed5a5cfe2adccb08e
Ufuk Beyaztas <58028887+UfukBeyaztas@users.noreply.github.com>
2022-06-10 11:38:22 +0300

    Update get.fpqr.coeffs.R

--- R/get.fpqr.coeffs.R
+++ R/get.fpqr.coeffs.R
@@ -1,12 +1,12 @@
-get.fpqr.coeffs <- function(object)
+get.fpqr.coeff <- function(object)
 {
-  sinp_mat <- object$model.details[[4]]
-  evalbase <- object$model.details[[5]]
-  gp <- object$model.details[[7]]
+  sinp_mat <- object$model.details$sinp_mat
+  evalbase <- object$model.details$evalbase
+  gp <- object$model.details$gp
   V <- object$V
   coeffs <- as.matrix(object$pqr.coefs[-1])
-  
-  f.coeff <- evalbase %*% (solve(sinp_mat) %*% V %*% coeffs)
-  
+
+  f.coeff <- evalbase %*% (t(solve(sinp_mat)) %*% V %*% coeffs)
+
   return(f.coeff)
 }
