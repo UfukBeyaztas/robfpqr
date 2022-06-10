e430a5122c436256869048d1c2ccdbca31687fa0
Ufuk Beyaztas <58028887+UfukBeyaztas@users.noreply.github.com>
2022-06-10 11:40:48 +0300

    Update predict_fpqr.R

--- R/predict_fpqr.R
+++ R/predict_fpqr.R
@@ -1,15 +1,15 @@
 predict_fpqr <- function(object, xnew)
 {
-  nbasis <- object$model.details[[6]]
-  gp <- object$model.details[[7]]
-  method.type <- object$model.details[[8]]
+  nbasis <- object$model.details$nbasis
+  gp <- object$model.details$gp
+  method.type <- object$model.details$method.type
   coeffs <- object$coeffs
   intercept <- object$intercept
-  
+
   BS.sol.test <- getAmat(data = xnew, nbf = nbasis, gp = gp)
   Amat.test <- BS.sol.test$Amat
   Amat.test <- scale_fun(data = Amat.test, method.type = method.type)
   predictions <- Amat.test %*% coeffs + intercept
-  
+
   return(predictions)
 }
